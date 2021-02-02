228a560cc125db8aadfda8ba3ea5fda4b2a99b97
kumarshashank571 <66532738+kumarshashank571@users.noreply.github.com>
2021-02-03 00:36:36 +0530

    Refactor : added heading

--- next-image-gallery/pages/index.js
+++ next-image-gallery/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Box } from "@chakra-ui/react";
+import { Box, Container, Text } from "@chakra-ui/react";
 export default function Home() {
   return (
     <div>
@@ -7,7 +7,21 @@ export default function Home() {
         <title> NextJS Image Gallery</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box overflow="hidden" bg="purple.300" minH="100vh"></Box>
+      <Box overflow="hidden" bg="purple.300" minH="100vh">
+      <Container>
+  <Text
+    color="green.800"
+    fontWeight="semibold"
+    mb="1rem"
+    textAlign="center"
+    textDecoration="underline"
+    fontSize={["4xl", "4xl", "5xl", "5xl"]}
+  >
+    NextJS Image Gallery
+  </Text>
+</Container>
+</Box>
+
     </div>
   );
 }
