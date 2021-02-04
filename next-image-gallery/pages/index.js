d0ba18ba98bc0ecbd4e5473b1c5e9aceb5771ad1
kumarshashank571 <66532738+kumarshashank571@users.noreply.github.com>
2021-02-05 01:47:17 +0530

    Feat : added image search feature

--- next-image-gallery/pages/index.js
+++ next-image-gallery/pages/index.js
@@ -1,11 +1,42 @@
 import Head from "next/head";
 //import { Box, Container, Text } from "@chakra-ui/react";
-import { Box, Container, Text, Wrap, WrapItem } from "@chakra-ui/react";
-import Image from "next/image";
+import {
+  Box,
+  Container,
+  Text,
+  Wrap,
+  WrapItem,
+  Input,
+  IconButton,
+  InputRightElement,
+  InputGroup,
+  useToast
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";import Image from "next/image";
 import React, { useState } from "react";
-import {getCuratedPhotos} from "../lib/api"
+//import {getCuratedPhotos} from "../lib/api"
+import { getCuratedPhotos, getQueryPhotos } from "../lib/api";
 export default function Home({data}) {
   const [photos, setPhotos] = useState(data);
+  const [query, setQuery] = useState("");
+  const toast = useToast();
+  const handleSubmit = async (e) => {
+    await e.preventDefault();
+    if (query == "") {
+      toast({
+        title: "Error.",
+        description: "Empty Search",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
+    } else {
+      const res = await getQueryPhotos(query);
+      await setPhotos(res);
+      await setQuery("");
+    }
+  };
   return (
     <div>
       <Head>
@@ -24,10 +55,42 @@ export default function Home({data}) {
   >
     NextJS Image Gallery
   </Text>
+  <form onSubmit={handleSubmit}>
+  <InputGroup pb="1rem">
+    <Input
+      placeholder="Search for Apple"
+      variant="ghost"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+
+    <InputRightElement
+      children={
+        <IconButton
+          aria-label="Search"
+          icon={<SearchIcon />}
+          onClick={handleSubmit}
+          bg="pink.400"
+          color="white"
+        />
+      }
+    />
+  </InputGroup>
+</form>
 </Container>
 <Wrap px="1rem" spacing={4} justify="center">
   {photos.map((pic) => (
-    <Image src={pic.src.portrait} height={600} width={400} alt={pic.url} />
+    <WrapItem
+      key={pic.id}
+      boxShadow="base"
+      rounded="20px"
+      overflow="hidden"
+      bg="white"
+      lineHeight="0"
+      _hover={{ boxShadow: "dark-lg" }}
+    >
+      <Image src={pic.src.portrait} height={600} width={400} alt={pic.url} />
+    </WrapItem>
   ))}
 </Wrap>
 </Box>
